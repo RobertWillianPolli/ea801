3586972a2553f81a40b982ccf6026f2c07e94bf7
RobertWillianPolli <60232212+RobertWillianPolli@users.noreply.github.com>
2026-04-27 09:16:57 -0300

    Update main.py

--- Projeto2/main.py
+++ Projeto2/main.py
@@ -2,28 +2,28 @@ import serial
 import time
 import kociemba
 
-# Ajuste a porta conforme seu sistema
-# Windows: COM3, COM4...
-# Linux/Mac: /dev/ttyUSB0 ou /dev/ttyACM0
-porta = 'COM10'  # ou '/dev/ttyUSB0'
-baud_rate = 9600
+# Ajuste da porta de comunicação
+porta = 'COM10'         # Windows
+baud_rate = 9600        # Taxa de comunicação
 
-arduino = serial.Serial(porta, baud_rate, timeout=1)
+# Cria instância do Arduino 
+arduino = serial.Serial(porta, baud_rate, timeout = 1)       
 
 time.sleep(2)  # aguarda inicialização do Arduino
-
-arduino.write(b'\n')  # manda ENTER inicial
-
 
 for faces in ["Superior", "Direita", "Frontal", "Inferior", "Esquerda", "Traseira"]:
         
     print(f"Posicione a face {faces} em frente à câmera e pressione ENTER")
     input()
         
-    if arduino.in_waiting > 0:
-        dado_lido = arduino.readline().decode('utf-8').strip()
-            
+    if arduino.in_waiting > 0:  # Aguarda recebimento de bytes do Arduino      
+
+        # Lê o dado do Arduino, decodifica e remove caracteres especiais
+        dado_lido = arduino.readline().decode('utf-8').strip()  
+        # Atualiza string de faces do cubo     
         facesDado += dado_lido        
-        
+
+# Envia a sequência de cores para o solver
 solution = kociemba.solve(scrambled_cube)
+# Exibe a solução
 print(f"Solution: {solution.split(' ')}")
